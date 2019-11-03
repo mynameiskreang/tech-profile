adb95fb3e1f056b0210a254aac62c41df52fc4d0
kreangks <kreangks@bot.or.th>
2019-11-03 12:30:07 +0700

    add view engine 'hbs' & sample index

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,10 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Query, Render } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Render('index.hbs')
+  index(@Query() query): object {
+    return { title: 'K-Stack', data: `Hello ${query.name || 'world'}` };
   }
 }
